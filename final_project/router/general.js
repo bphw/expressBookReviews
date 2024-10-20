087d31f56bfc5c2cadc39bf1f9653a57c7ce200c
Bambang Wicaksono <bambanx@live.com>
2024-10-20 04:02:26 -0400

    Update Task2 - getdetailsISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,13 +13,27 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+//   Task1
+//   return res.status(300).json({message: "Yet to be implemented"});
+    return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Task2
+  const isbn = req.params.isbn;
+  let book = books.filter((item) => 
+    item.hasOwnProperty('isbn') && item.isbn === isbn
+  );
+
+  if(book.length < 1) {
+    return res.status(404).json({message: `No book found with ISBN ${isbn}`});
+  } else {
+    return res.send(JSON.stringify(books[isbn]));
+  } 
+  
+//   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
