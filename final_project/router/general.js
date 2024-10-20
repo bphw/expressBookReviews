e6d55eaffdba79d11319775727f45a9b34d00057
Bambang Wicaksono <bambanx@live.com>
2024-10-20 05:35:12 -0400

    Update Task4 - getbooksbytitle

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,14 +23,24 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   // Task2
   const isbn = req.params.isbn;
-  let book = books.filter((item) => 
+  /* bellow if book has properties named 'isbn'
+   * but since in this project isbn refer to object number, 
+   * then simply use books[isbn]
+   
+  const arrayBooks = Object
+    .keys(books)
+    .map(key => books[key]);
+  
+  let book = arrayBooks.filter((item) => 
     item.hasOwnProperty('isbn') && item.isbn === isbn
   );
+   */
 
-  if(book.length < 1) {
-    return res.status(404).json({message: `No book found with ISBN ${isbn}`});
-  } else {
+  if(books[isbn]) {
     return res.send(JSON.stringify(books[isbn]));
+  } else {
+    return res.status(404).json({message: `No book found with ISBN ${isbn}`});
+    
   } 
   
 //   return res.status(300).json({message: "Yet to be implemented"});
@@ -39,12 +49,34 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
+  // Task3
+  const author = req.params.author;
+  let matchAuthor = [];
+
+  for(let key in books) {
+    console.log(books[key]);
+    if (books[key].author === author) matchAuthor.push(books[key]);
+  }
+  if(matchAuthor.length > 0) {
+    return res.status(200).json(matchAuthor);
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
+  // Task4
+  const title = req.params.title;
+  let matchTitle = [];
+
+  for(let key in books) {
+    console.log(books[key]);
+    if (books[key].title === title) matchTitle.push(books[key]);
+  }
+  if(matchTitle.length > 0) {
+    return res.status(200).json(matchTitle);
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
